b801af5fc3c6540c77a1242706b14bb48ba18fd5
minisera <koki.ohtaka0821@gmail.com>
2021-01-25 18:22:49 +0900

    feat: PostとUserがリレーションになるようPostマイグレーションファイル修正

--- database/migrations/2021_01_25_083739_create_posts_table.php
+++ database/migrations/2021_01_25_083739_create_posts_table.php
@@ -18,6 +18,11 @@ class CreatePostsTable extends Migration
             $table->timestamps();
             $table->string('title');
             $table->longText('body');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+
         });
     }
 
